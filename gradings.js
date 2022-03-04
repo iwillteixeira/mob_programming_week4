bebc13adda0aa0ca028ce14a1e0bea635ff4779a
iwillteixeira <iwill2005@gmail.com>
2022-03-04 12:10:43 -0300

    Solved gradings

---
+++ gradings.js
@@ -0,0 +1,26 @@
+let fiveMultiple = (grade) =>{
+    let counter = 0;
+    while (grade % 5 != 0) {
+        grade++;
+        counter++;
+    }
+    return counter;
+}
+
+function gradingStudents(grades) {
+    // Write your code here
+    let roundedGrades = [];
+    grades.forEach(e=>{
+        let difference = fiveMultiple(e);
+        let finalGrade = difference + e;
+        if ((difference < 3) && (finalGrade >= 40)) {
+            roundedGrades.push(finalGrade);
+        } else {
+            roundedGrades.push(e);
+        }
+    })
+    return roundedGrades;
+
+}
+
+console.log(gradingStudents([4,73,67,38,33]))
